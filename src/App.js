4c2ce5597d2a6532f53546fbae104b6fbd1e3032
k414bin <k414bin@yandex.ru>
2022-08-09 18:13:24 +0300

    Master branch recreate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
 
-import Message from './components/messages/message';
-
-function App(props) {
-    const topPosition = '50px';
+function App() {
   return (
     <div className="App">
-       <header className="App-header" style={{top: topPosition} ,{color: 'red'}}>
-        My First React App!        
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <body>
-        <Message messageText='This message from  '/> {props.name}
-      </body>
     </div>
   );
 }
